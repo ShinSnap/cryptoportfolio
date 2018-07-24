0a91a074310f589db9c521d69411f22772da90af
shinSnap <xshijimax@gmail.com>
2018-07-24 14:40:06 -0700

    Add coinmarketcap api v1

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,11 @@
 class HomeController < ApplicationController
   def index
+    require 'net/http'
+    require 'json'
+
+    url = 'https://api.coinmarketcap.com/v1/ticker/'
+    uri = URI(url)
+    response = Net::HTTP.get(uri)
+    @response = JSON.parse(response)
   end
 end
