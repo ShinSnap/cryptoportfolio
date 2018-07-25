72bbf482f5a5e60ae6ae24118f12d3c44c59a390
shinSnap <xshijimax@gmail.com>
2018-07-24 21:13:03 -0700

    Added lookup page and upgraded to api to v2

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,9 +3,34 @@ class HomeController < ApplicationController
     require 'net/http'
     require 'json'
 
-    url = 'https://api.coinmarketcap.com/v1/ticker/'
+    url = 'https://api.coinmarketcap.com/v2/ticker/'
     uri = URI(url)
     response = Net::HTTP.get(uri)
-    @response = JSON.parse(response)
+    @coins = JSON.parse(response)
+
+    @my_coins = [ "BTC", "XRP", "ETC", "XLM", "NEO", "WAX", "STEEM" ]
+  end
+
+  def lookup
+    require 'net/http'
+    require 'json'
+
+    url = 'https://api.coinmarketcap.com/v2/ticker/'
+    uri = URI(url)
+    response = Net::HTTP.get(uri)
+    @lookup_coin = JSON.parse(response)
+
+    @symbol = params[:sym]
+
+    if @symbol
+      @symbol = @symbol.upcase
+    end
+
+    if @symbol == ''
+      @symbol = "You forgot to enter a currency"
+    end
+  end
+
+  def aboutus
   end
 end
